9e273d481c43e01efdeb981d32a1e6c61128aa6d
datomaluta <datomaluta@gmail.com>
2024-04-09 15:32:35 +0400

    add mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+// datomaluta
+// 7U9jf9ehrOGTnJ0U
+
+// mongodb+srv://datomaluta:7U9jf9ehrOGTnJ0U@mern-blog.buhnglk.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog
