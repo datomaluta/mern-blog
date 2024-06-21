de3cd2f7dd0e45405d54f3c94f1ed401e5031418
datomaluta <datomaluta@gmail.com>
2024-06-21 18:10:13 +0400

    deploy: axios

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:5173/api",
-  // baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:5173/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     // "Content-Type": "multipart/form-data",
